877544f3df8c479ab71faca2072019e05f507952
Ben0112358 <111197969+Ben0112358@users.noreply.github.com>
2025-07-31 01:06:20 +0200

    Allow simultaneous different modes (#3)
    * add -p flag for each sibling docker container and add ports that are functions of project and mode
    
    * linting

--- ml-training.sh
+++ ml-training.sh
@@ -13,5 +13,5 @@ cd "${ML_WORKSPACE_ROOT}/ml-training" || {
 	exit 1
 }
 
-docker-compose -f "docker-compose.${PROJECT_NAME}.yaml" build --no-cache
-docker-compose -f "docker-compose.${PROJECT_NAME}.yaml" up -d
+docker-compose -f "docker-compose.${PROJECT_NAME}.yaml" -p "${PROJECT_NAME}_${MODE}" build --no-cache
+docker-compose -f "docker-compose.${PROJECT_NAME}.yaml" -p "${PROJECT_NAME}_${MODE}" up -d
