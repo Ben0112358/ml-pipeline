309fe2e67b740e2b3a10aa128ec58cea3c7213d2
Ben0112358 <benjaminringdahl@gmail.com>
2025-07-31 19:50:53 +0200

    add logging where suitable

--- ml-serving.sh
+++ ml-serving.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 set -euo pipefail
 
+source ./utils.sh
 PROJECT_NAME="${1:-}"
+log "INFO" "$0 executed for project $PROJECT_NAME"
 
 if [[ -z "$PROJECT_NAME" ]]; then
-	echo "Usage: $0 <project_name>"
+	log "ERROR" "Usage: $0 <project_name>"
 	exit 1
 fi
 
 cd "${ML_WORKSPACE_ROOT}/ml-serving" || {
-	echo "Failed to cd into ml-serving repo."
+	log "ERROR" "Failed to cd into ml-serving repo."
 	exit 1
 }
 
+log "INFO" "Docker composing."
 docker-compose -f "docker-compose.${PROJECT_NAME}.yaml" -p "${PROJECT_NAME}_${MODE}" build --no-cache
 docker-compose -f "docker-compose.${PROJECT_NAME}.yaml" -p "${PROJECT_NAME}_${MODE}" up -d
--- pipeline.sh
+++ pipeline.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
-set -euo
+set -euo pipefail
 
+source ./utils.sh
 PROJECT_NAME="$1"
 MODE="$2"
+OUTPUT_SUFFIX="$3"
+log "INFO" "Running $0 with args: $1, $2."
 
-echo "---Running setup---"
+log "INFO" "Running setup."
 ./setup.sh "$MODE"
 
-echo "---Running infra---"
+log "INFO" "Setup succeeded => rename log file from unknown to the known project name."
+NEW_LOG_FILE_PATH="${ML_HOMELAB_ROOT}/log_${OUTPUT_SUFFIX}.log"
+mv "${LOG_FILE_PATH}" "${NEW_LOG_FILE_PATH}"
+LOG_FILE_PATH=$NEW_LOG_FILE_PATH
+
+log "INFO" "Running ml-infra."
 ./ml-infra.sh "$PROJECT_NAME" "$MODE"
 
-echo "---Running data---"
+log "INFO" "Running ml-data."
 ./ml-data.sh "$PROJECT_NAME"
 
-echo "---Running training---"
+log "INFO" "Running ml-training."
 ./ml-training.sh "$PROJECT_NAME"
 
-echo "---Running serving---"
+log "INFO" "Running serving."
 ./ml-serving.sh "$PROJECT_NAME"
 
-echo "---Running ui---"
+log "INFO" "Running ml-ui."
 ./ml-ui.sh "$PROJECT_NAME"
