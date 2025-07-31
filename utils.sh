46e7582991c444fb3330c5127cd9d0ef7dd17aad
Ben0112358 <benjaminringdahl@gmail.com>
2025-07-31 19:57:00 +0200

    change date format

--- utils.sh
+++ utils.sh
@@ -7,7 +7,7 @@ log() {
 	local severity="$1"
 	local msg="$2"
 
-	local full_row="$(date -u) - $severity: $msg"
+	local full_row="$(date -u +"%a %b %d %T UTC %Y") - $severity: $msg"
 	echo "$msg"
 	echo "$full_row" >>"${LOG_FILE_PATH}"
 }
